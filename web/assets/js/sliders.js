6551c67caa77d9166675f92990e3aa06a9ab1a29
jennifer <jennifer@ambitious.co.nz>
2024-02-27 12:42:31 +1300

    progress

---
+++ web/assets/js/sliders.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper('.slider-environments', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  //   autoplay: {
+  //     delay: 8000,
+  //     disableOnInteraction: false,
+  //   },
+
+  breakpoints: {
+    1024: {
+      slidesPerView: 1.7,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+
+$(document).ready(function () {
+  $('.slider-environments h3').each(function () {
+    var text = $(this).text()
+    var newText = text.replace(/(Urban)(\w*)/g, '$1<span><i>$2</i></span>')
+    $(this).html(newText)
+  })
+})
