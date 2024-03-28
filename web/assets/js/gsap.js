605c3c6564857b80ddafeace12b5003c5eeea995
jennifer <jennifer@ambitious.co.nz>
2024-03-28 16:42:29 +1300

    our space animation started

---
+++ web/assets/js/gsap.js
@@ -0,0 +1,17 @@
+gsap.registerPlugin(ScrollTrigger)
+
+let tl = gsap.timeline({
+  defaults: { duration: 1, ease: 'circ.out' },
+
+  scrollTrigger: {
+    trigger: '.hero-animation',
+    // pin: true,
+    start: 'top top+=50',
+    end: 'center top',
+    scrub: 1,
+    invalidateOnRefresh: true,
+    markers: { startColor: 'blue', endColor: 'red', fontSize: '18px', fontWeight: 'bold', indent: 20 },
+  },
+})
+
+tl.to('.hero-cloud', { yPercent: -100 })
