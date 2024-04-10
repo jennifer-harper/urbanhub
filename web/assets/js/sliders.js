91d072a59d8b835c4964ba091aad53d0c69e93ee
jennifer <jennifer@ambitious.co.nz>
2024-04-11 11:07:45 +1200

    pre nav save

--- web/assets/js/sliders.js
+++ web/assets/js/sliders.js
@@ -1,3 +1,4 @@
+//urban solutions pages
 var swiper = new Swiper('.slider-urban-solutions', {
   slidesPerView: 1,
   spaceBetween: 0,
@@ -21,10 +22,21 @@ var swiper = new Swiper('.slider-urban-solutions', {
   },
 })
 
+//putting a space in the name/url slide.spaceType[0]
 $(document).ready(function () {
   $('.slider-environments h3').each(function () {
     var text = $(this).text()
     var newText = text.replace(/(Urban)(\w*)/g, '$1<span><i>$2</i></span>')
     $(this).html(newText)
   })
+})
+
+var swiper = new Swiper('.slider-testimonials', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
 })
