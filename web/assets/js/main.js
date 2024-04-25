9d26320c9fff225b2e955221bad2c1ab108444b1
DDEV User <nobody@example.com>
2024-04-25 22:22:00 +0000

    changes

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -7,23 +7,27 @@
 //     $(this).hide() // hide loading div
 //   })
 
-// $(document).ready(function () {
-//   $.ajax({
-//     type: 'GET',
-//     url: 'urbanhub.ddev.site', // Replace with your URL
-//     beforeSend: function () {
-//       $('#loadingDiv').fadeIn(300)
-//     },
-//     success: function (data) {
-//       console.log(data)
-//     },
-//     complete: function () {
-//       setTimeout(function () {
-//         $('#loadingDiv').fadeOut(300)
-//       }, 500)
-//     },
-//   })
-// })
+//////////////
+
+$(document).ready(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'urbanhub.ddev.site', // Replace with your URL
+    beforeSend: function () {
+      $('#loadingDiv').fadeIn(300)
+    },
+    success: function (data) {
+      console.log(data)
+    },
+    complete: function () {
+      setTimeout(function () {
+        $('#loadingDiv').fadeOut(300)
+      }, 500)
+    },
+  })
+})
+
+///////////////////////////////////
 
 // $(document).ajaxStart(function () {
 //   $('#loadingDiv').fadeIn(300)
@@ -45,17 +49,3 @@
 //     },
 //   })
 // })
-
-$(document).ready(function () {
-  // Show loading animation
-  $('#loadingDiv').fadeIn(300)
-
-  // Wait for the page to fully load
-  $(window).on('load', function () {
-    // Hide loading animation
-    $('#loadingDiv').fadeOut(300)
-
-    // Show the main content
-    $('main').fadeIn(300)
-  })
-})
