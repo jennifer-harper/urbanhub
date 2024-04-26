47317f07fdc43b4ba95bd55331531cbb60ad764d
DDEV User <nobody@example.com>
2024-04-26 00:19:46 +0000

    see load time

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,15 +1,32 @@
 //////////////
 window.onload = function () {
   $('#prep').css('display', 'none')
+  var startTime = performance.now() // Record start time
 
   $.ajax({
     type: 'GET',
     url: 'urbanhub.frb.io',
     complete: function () {
-      //   setTimeout(function () {
-      $('#loadingDiv').fadeOut()
+      var endTime = performance.now() // Record end time
+      var loadingTime = endTime - startTime // Calculate loading time in milliseconds
+      console.log('Local Site Loading Time:', loadingTime.toFixed(2), 'ms')
+      setTimeout(function () {
+        $('#loadingDiv').fadeOut(100)
+        $('#prep').css('display', 'block')
+      }, 300)
+    },
+  })
+
+  $.ajax({
+    type: 'GET',
+    url: 'urbanhub.ddev.site',
+    complete: function () {
+      var endTime = performance.now() // Record end time
+      var loadingTime = endTime - startTime // Calculate loading time in milliseconds
+      console.log('Live Site Loading Time:', loadingTime.toFixed(2), 'ms')
+
+      $('#loadingDiv').fadeOut(100)
       $('#prep').css('display', 'block')
-      //   }, 500)
     },
   })
 }
