6cbe5444864e26f32ef651dc4d3fc566853a3313
DDEV User <nobody@example.com>
2024-04-26 00:28:08 +0000

    testing specifics

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -3,23 +3,23 @@ window.onload = function () {
   $('#prep').css('display', 'none')
   var startTime = performance.now() // Record start time
 
-  $.ajax({
-    type: 'GET',
-    url: 'urbanhub.frb.io',
-    complete: function () {
-      var endTime = performance.now() // Record end time
-      var loadingTime = endTime - startTime // Calculate loading time in milliseconds
-      console.log('Local Site Loading Time:', loadingTime.toFixed(2), 'ms')
-      setTimeout(function () {
-        $('#loadingDiv').fadeOut(100)
-        $('#prep').css('display', 'block')
-      }, 300)
-    },
-  })
+  //   $.ajax({
+  //     type: 'GET',
+  //     url: 'https://urbanhub.ddev.site/',
+  //     complete: function () {
+  //       var endTime = performance.now() // Record end time
+  //       var loadingTime = endTime - startTime // Calculate loading time in milliseconds
+  //       console.log('Local Site Loading Time:', loadingTime.toFixed(2), 'ms')
+  //       setTimeout(function () {
+  //         $('#loadingDiv').fadeOut(100)
+  //         $('#prep').css('display', 'block')
+  //       }, 300)
+  //     },
+  //   })
 
   $.ajax({
     type: 'GET',
-    url: 'urbanhub.ddev.site',
+    url: 'https://urbanhub.frb.io',
     complete: function () {
       var endTime = performance.now() // Record end time
       var loadingTime = endTime - startTime // Calculate loading time in milliseconds
