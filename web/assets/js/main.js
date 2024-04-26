d865c0a51781287a072b96c0f3f7a259cbd3a77f
jennifer <jennifer@ambitious.co.nz>
2024-04-26 12:01:23 +1200

    testing page load

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,25 +1,14 @@
-// $('#loadingDiv')
-//   .hide()
-//   .ajaxStart(function () {
-//     $(this).show() // show Loading Div
-//   })
-//   .ajaxStop(function () {
-//     $(this).hide() // hide loading div
-//   })
-
 //////////////
 window.onload = function () {
   $('#prep').css('display', 'none')
-  //   $('#prep').hide()
 
   $.ajax({
     type: 'GET',
     complete: function () {
-      setTimeout(function () {
-        $('#loadingDiv').fadeOut(100)
-        $('#prep').css('display', 'block')
-        // $('#prep').show()
-      }, 500)
+      //   setTimeout(function () {
+      $('#loadingDiv').fadeOut(100)
+      $('#prep').css('display', 'block')
+      //   }, 500)
     },
   })
 }
