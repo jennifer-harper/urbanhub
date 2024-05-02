f010033883c494a119ed50c88a13dc8adbe7679d
jennifer <jennifer@ambitious.co.nz>
2024-05-02 15:38:34 +1200

    basic animations finsihed, except birds

--- web/assets/js/building.js
+++ web/assets/js/building.js
@@ -1,6 +1,6 @@
 // use a script tag or an external JS file
 document.addEventListener('DOMContentLoaded', (event) => {
-  gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother, MotionPathPlugin)
   const smoother = ScrollSmoother.create({
     wrapper: '#smooth-wrapper',
     content: '#smooth-content',
@@ -20,6 +20,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
       scrollTrigger: {
         trigger: '.firstSection',
         pin: true,
+        anticipatePin: 1,
         scrub: 1,
         start: 'top top+=' + navHeight,
         end: 'bottom top',
@@ -44,20 +45,31 @@ document.addEventListener('DOMContentLoaded', (event) => {
         start: 'top top+=' + navHeight,
         end: 'bottom top',
         pin: true,
-        scrub: true,
+        anticipatePin: 1,
+        scrub: 1,
         invalidateOnRefresh: true,
-        // markers: marker
+        markers: marker,
       },
     })
     gsap.set('.secondSection .content', { yPercent: 150 })
     gsap.set('.secondSection .light', { alpha: 0 })
+    gsap.set('.secondSection .bird', { alpha: 0 })
     tl.addLabel('start')
-    tl.to('.secondSection .bird', { yPercent: -100, xPercent: 250 }, 'start')
+    // tl.to('.secondSection .bird', {
+    //   duration: 4,
+    //   motionPath: {
+    //     path: '#mainPath',
+    //     align: '#mainPath',
+    //     alignOrigin: [0.5, 0.5],
+    //     autoRotate: true,
+    //   },
+    // })
+
     tl.to('.secondSection .light', { alpha: 0.5, duration: 0.1 }, 'start+=1.5')
     tl.to('.secondSection .light', { alpha: 0, duration: 0.1 }, 'start+=2')
     tl.to('.secondSection .light', { alpha: 0.7, duration: 0.1 }, 'start+=2.1')
     tl.to('.secondSection .light', { alpha: 0, duration: 0.1 }, 'start+=2.2')
-    tl.to('.secondSection .light', { alpha: 0.95, duration: 0.1 }, 'start+=2.3')
+    tl.to('.secondSection .light', { alpha: 0.9, duration: 0.1 }, 'start+=2.3')
     tl.to('.secondSection .content', { yPercent: 0, ease: 'circ.out' }, 'start')
   }
 
@@ -69,22 +81,66 @@ document.addEventListener('DOMContentLoaded', (event) => {
         start: 'top top+=' + navHeight,
         end: 'bottom top',
         pin: true,
-        scrub: true,
+        anticipatePin: 1,
+        scrub: 1,
         invalidateOnRefresh: true,
-        markers: marker,
+        // markers: marker,
       },
     })
     gsap.set('.thirdSection .content', { yPercent: 150 })
+    gsap.set('.thirdSection .wind', { xPercent: -200, yPercent: 300, scale: 0 })
     tl.addLabel('start')
     tl.to('.thirdSection .minute', { rotate: 360 }, 'start')
     tl.to('.thirdSection .hour-hand', { rotate: 20, duration: 1 }, 'start+=2')
     tl.to('.thirdSection .content', { yPercent: 0, ease: 'circ.out' }, 'start')
+    tl.to('.thirdSection .wind', { xPercent: 0, yPercent: 0, scale: 1, ease: 'circ.out' }, 'start')
+  }
+
+  function fourthSection() {
+    let tl = gsap.timeline({
+      defaults: { duration: 4, ease: 'circ.out' },
+      scrollTrigger: {
+        trigger: '.fourthSection',
+        start: 'top top+=' + navHeight,
+        end: 'bottom top',
+        pin: true,
+        anticipatePin: 1,
+        scrub: 1,
+        invalidateOnRefresh: true,
+        toggleActions: 'play pause resume reset',
+        // markers: marker,
+      },
+    })
+    gsap.set('.fourthSection .content', { yPercent: 150 })
+    gsap.set('.fourthSection .bird', { alpha: 0 })
+    tl.addLabel('start')
+    // tl.to('.fourthSection .bird', { alpha: 1 }, 'start')
+    tl.to('.fourthSection .content', { yPercent: 0 }, 'start')
+    tl.to('.fourthSection .wave', { xPercent: -10, scale: 1.5, stagger: 0.5, duration: 1.5 }, 'start')
+    tl.to('.fourthSection .wave', { xPercent: 0, scale: 1, stagger: -0.5, duration: 1.5 }, 'start+=2')
+  }
+
+  function fifthhSection() {
+    let tl = gsap.timeline({
+      defaults: { duration: 1, ease: 'none' },
+      scrollTrigger: {
+        trigger: '.fifthSection',
+        start: 'top top+=' + navHeight,
+        end: 'bottom bottom',
+        toggleActions: 'play none none reverse',
+        // markers: marker,
+      },
+    })
+    gsap.set('.fifthSection .floor-light', { alpha: 0 })
+    tl.to('.fifthSection .floor-light', { alpha: 1, duration: 0 })
   }
 
   mm.add('(min-width: 1024px)', () => {
     firstSection()
     secondSection()
     thirdSection()
+    fourthSection()
+    fifthhSection()
   })
 
   mm.add('(max-width: 1023px)', () => {
@@ -95,6 +151,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
     gsap.set('.secondSection .light', { alpha: 1 })
     //third
     gsap.set('.thirdSection .content', { yPercent: 0 })
+    gsap.set('.thirdSection .wind', { xPercent: 0 })
+    //fourth
+    gsap.set('.fourthSection .content', { yPercent: 0 })
+    //fifth
+    gsap.set('.fifthSection .floor-light', { alpha: 0 })
   })
 })
 
