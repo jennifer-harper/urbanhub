7973b83366af9bd701b54187ac19c368c84fcb72
jennifer <jennifer@ambitious.co.nz>
2024-05-09 16:29:06 +1200

    building animation mediamatch

--- web/assets/js/building.js
+++ web/assets/js/building.js
@@ -27,8 +27,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
   })
 
   let mm = gsap.matchMedia()
-  var navHeight = $('nav').innerHeight()
   var marker = { startColor: 'green', endColor: 'red', fontSize: '18px', fontWeight: 'bold', indent: 20, zIndex: 2000 }
+
+  // Function to recalculate navHeight on window resize
+  function updateNavHeight() {
+    let nav = $('.logo').innerHeight()
+    navHeight = nav + 20
+    console.log(navHeight)
+    ScrollTrigger.refresh()
+  }
+  updateNavHeight()
+  // Call the updateNavHeight function on window resize
+  window.addEventListener('resize', updateNavHeight)
 
   function firstSection() {
     let tl = gsap.timeline({
